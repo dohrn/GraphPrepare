7b0a17c9d9702ff2d3e51cb2f1645dbd425c8725
Dennis <d.ohrndorf@gmail.com>
2019-11-30 23:53:51 +0100

    Integral Changes

--- Graphprepare.py
+++ Graphprepare.py
@@ -3,29 +3,32 @@ import time
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import pairwise_distances
 import scipy.sparse
+import numpy as np
 
-dataset_name = input("Bitte Namen des CSV-Dataset eingeben (z.B. Data.csv):   ")
+
+
+dataset_name = "Input_CSV/{}".format(input("Bitte Namen des CSV-Dataset eingeben (z.B. Data.csv):   "))
 column = int(input("Welche Spalte soll betrachtet werden? [0 = erste Spalte]:    "))
+to_mask = int(input("Soll der Datensatz gekürzt werden? [1/0]:   "))
+
+if to_mask == 1:
+	mask_weight = float(input("Welches Verhältnis? [z.B. 0.5]:    "))
+else:
+	mask_weight = 1
+
 
 # try:
 # 	data = pd.read_csv(dataset_name, error_bad_lines=False)
 # except:
 # 	data = pd.read_csv(dataset_name, error_bad_lines=False, sep=";")
-data = pd.read_csv(dataset_name, error_bad_lines=False, sep=";")
-print("read_csv - check")
-# if int(column) == 1:
-# 	working_data = data["Document Title"]
-# 	subtext = "Document Title"
-# elif int(column) == 2:
-# 	working_data = data["Author Keywords"]
-# 	subtext = "Author Keywords"
-# elif int(column) == 3:
-# 	working_data = data["Abstract"]
-# 	subtext = "Abstract"
-# elif int(column) == 4:
-# 	working_data = data["Authors"]
-# 	subtext = "Authors"
 
+data = pd.read_csv(dataset_name, error_bad_lines=False)
+print("read_csv - check")
+mask = np.random.rand(len(data)) <= mask_weight
+
+data = data[mask]
+
+print(len(data))
 
 subtext = str(data.columns[column])
 working_data = data[subtext]
@@ -33,21 +36,26 @@ working_data = data[subtext]
 print("Betrachte Spalte {}".format(subtext))
 working_data = working_data.dropna()
 
-vectorizer = CountVectorizer(ngram_range=(1,1), stop_words = 'english')
-X = vectorizer.fit_transform(working_data)
+max_features_working_data = int(4 * 10 ** 9 / (80*len(data))) - 2
+
+vectorizer = CountVectorizer(ngram_range=(1,1), stop_words = 'english', max_features = max_features_working_data).fit(working_data)
+X = vectorizer.transform(working_data)
 print("CountVectorizer - check")
 names = vectorizer.get_feature_names()
+print(X)
 array_data = X.toarray()
+
+
 new_df = pd.DataFrame(data = array_data, columns = names)
 print("starting JacSim")
 #np_array = scipy.sparse.csr_matrix(new_df.T.to_numpy())
 np_array = new_df.T
-jac_sim = 1 - pairwise_distances(np_array, metric = "jaccard")
+jac_sim = 1 - pairwise_distances(np_array, metric = "cosine")
 jac_sim = pd.DataFrame(jac_sim, index=new_df.columns, columns=new_df.columns)
 print("JacSim - check")
 
-top = int(0.2*len(jac_sim))
-jac_sim = jac_sim.mask(jac_sim.rank(axis=0, method='min', ascending=False) > top, 0)
+#top = int(0.2*len(jac_sim))
+#jac_sim = jac_sim.mask(jac_sim.rank(axis=0, method='min', ascending=False) > top, 0)
 print("JacSim_Reduce - check")
 output_file_name = "{}_{}_GraphInput.csv".format(dataset_name[:-4], subtext)
 jac_sim.to_csv(output_file_name)
